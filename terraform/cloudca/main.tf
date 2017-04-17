e3ce0dd3c07ff02ec4b5c75413ea70862d435f24
Ayrat Khayretdinov <akhayretdinov@cloudops.com>
2017-04-17 18:14:29 -0400

    Added variables
    Variables:
    Storage
    Vms

--- terraform/cloudca/main.tf
+++ terraform/cloudca/main.tf
@@ -2,25 +2,28 @@ provider "cloudca" {
   api_key = "${var.api_key}"
 }
 
-resource "cloudca_instance" lab-1 {
+resource "cloudca_instance" node {
    environment_id = "${var.environment_id}"
-   name="lab-1"
-   network_id="fe2fbac4-1c2e-4dd6-8095-e4e2156f7d8a"
-   template = "Ubuntu 16.04.01 HVM"
-   compute_offering = "2vCPU.8GB"
+   count="${var.vm_count}"
+   name="${format("helm%1d", count.index + 1)}"
+   network_id="${var.network_id}"
+   template = "${var.image_name}"
+   compute_offering = "${var.vm_flavor}"
    ssh_key_name = "${var.key_name}"
 }
 
 resource "cloudca_volume" drive_1 {
    environment_id = "${var.environment_id}"
-   name = "drive_1"
-   disk_offering = "20GB - 20 IOPS Min."
-   instance_id="${cloudca_instance.lab-1.id}"
+   count = "${var.vol1_count}"
+   name = "docker_${element(cloudca_instance.node.*.name, count.index)}"
+   disk_offering = "${var.vol1_size}"
+   instance_id   = "${element(cloudca_instance.node.*.id, count.index)}"
 }
 
 resource "cloudca_volume" drive_2 {
    environment_id = "${var.environment_id}"
-   name = "drive_2"
-   disk_offering = "20GB - 20 IOPS Min."
-   instance_id="${cloudca_instance.lab-1.id}"
+   count = "${var.vol2_count}"
+   name = "nfsprovisioner_${element(cloudca_instance.node.*.name, count.index)}"
+   disk_offering = "${var.vol2_size}"
+   instance_id   = "${element(cloudca_instance.node.*.id, count.index)}"
 }
--- terraform/cloudca/variables.tf
+++ terraform/cloudca/variables.tf
@@ -12,3 +12,45 @@ variable "key_name"{
   description = "Your Cloud.ca ssh key pair key"
   type="string"
 }
+
+#OpenStack Helm nodes variables
+variable vm_count {
+  description = "Number of Helm Openstack VMs"
+  default     = 0
+}
+
+variable network_id {
+  description = "Standard Tier Network ID"
+  type="string"
+}
+
+variable image_name {
+  description = "Base OS for VMs running K8s"
+  default     = "Ubuntu 16.04.01 HVM"
+}
+
+variable vm_flavor {
+  description = "Size of VMs running K8s Master"
+  default     = "4vCPU.16GB"
+}
+
+#STORAGE part
+variable vol1_count {
+  description = "Number of volumes for Storage VM"
+  default     = 0
+}
+
+variable vol1_size {
+  description = "Size of Volumes for Persistant Storage"
+  default     = "20GB - 20 IOPS Min."
+}
+
+variable vol2_count {
+  description = "Number of volumes for Storage VM"
+  default     = 0
+}
+
+variable vol2_size {
+  description = "Size of Volumes for Persistant Storage"
+  default     = "200GB - 2000 IOPS Min."
+}
