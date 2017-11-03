7859e4db49accd25a99addfd292f6f7548645f36
archyufa <ayrat.khayretdinov@gmail.com>
2017-11-03 11:59:08 -0400

    terraform modules

---
+++ terraform/arbor-wksp/cloudinit.tpl
@@ -0,0 +1,19 @@
+#cloud-config
+apt:
+  sources:
+    docker.list:
+      source: 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+      keyid: 5811 8E89 F3A9 1289 7C07 0ADB F762 2157 2C52 609D
+      keyserver: 'hkp://p80.pool.sks-keyservers.net:80'
+packages:
+  - docker-engine
+  - python-simplejson
+runcmd:
+  - usermod -aG docker cca-user
+  - systemctl start docker
+  - systemctl enable docker
+  - 'curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine'
+  - 'chmod +x /tmp/docker-machine'
+  - 'sudo cp /tmp/docker-machine /usr/local/bin/docker-machine'
+  - 'sudo curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+  - 'sudo chmod +x /usr/local/bin/docker-compose'
---
+++ terraform/bell-docker/main.tf
@@ -0,0 +1,39 @@
+provider "cloudca" {
+  api_key                 = "${var.api_key}"
+}
+
+# Create the Public IP for Helm nodes.
+resource "cloudca_public_ip" "master_ip_endpoint" {
+  count                   = "${var.vm_count}"
+  environment_id          = "${var.environment_id}"
+  vpc_id                  = "${var.cloudca_vpc}"
+}
+
+# cloudinit
+data "template_file" "cloudinit" {
+  template = "${file("./cloudinit.tpl")}"
+}
+
+# Create VM for Workshop Nodes
+resource "cloudca_instance" node {
+   environment_id         = "${var.environment_id}"
+   count                  = "${var.vm_count}"
+   name                   = "${format("docker-student%1d", count.index + 1)}"
+   network_id             = "${var.network_id}"
+   template               = "${var.image_name}"
+   compute_offering       = "${var.vm_flavor}"
+   ssh_key_name           = "${var.key_name}"
+   user_data              = "${data.template_file.cloudinit.rendered}"
+   root_volume_size_in_gb = "${var.root_size}"
+}
+
+resource "cloudca_static_nat" "_nat" {
+   count                  = "${var.vm_count}"
+   environment_id         = "${var.environment_id}"
+   public_ip_id           = "${element(cloudca_public_ip.master_ip_endpoint.*.id, count.index)}"
+   private_ip_id          = "${element(cloudca_instance.node.*.private_ip_id, count.index)}"
+}
+
+output "node_name" {
+  value                   = "${cloudca_instance.node.name}"
+}
